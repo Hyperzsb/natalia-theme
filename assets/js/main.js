ed5cd1c48a5b7bda33ed782249c2035ee957415c
Hyperzsb <hyperzsb@qq.com>
2022-02-27 18:39:00 +0800

    feat(loading modal): add basic loading modal

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,12 +6,14 @@ function changeNavbarShadow() {
         if (!classList.contains("custom-navbar-shadow-color"))
             classList.add("custom-navbar-shadow-color");
         if (classList.contains("p-4"))
-            classList.replace("p-4", "p-2");
+            //classList.replace("p-4", "p-2");
+            classList.remove("p-4")
     } else {
         if (classList.contains("custom-navbar-shadow-color"))
             classList.remove("custom-navbar-shadow-color");
-        if (classList.contains("p-2"))
-            classList.replace("p-2", "p-4");
+        if (!classList.contains("p-4"))
+            //classList.replace("p-2", "p-4");
+            classList.add("p-4")
     }
 }
 
@@ -23,6 +25,33 @@ function changeNavbarTogglerIcon() {
         else
             classList.replace("bi-arrows-collapse", "bi-arrows-expand");
     })
+}
+
+document.onreadystatechange = function () {
+    if (document.readyState === "complete") {
+        let loadingModal = document.getElementById("loading-modal");
+        if (loadingModal != null) {
+            let degree = 5;
+            let interval = setInterval(function () {
+                degree--;
+                loadingModal.style.opacity = String(degree / 5);
+                if (degree === 0) {
+                    loadingModal.style.display = "none";
+                    clearInterval(interval);
+                }
+            }, 50);
+        }
+        let mainContent = document.getElementById("main-content");
+        mainContent.style.display = "block";
+        let degree = 0;
+        let interval = setInterval(function () {
+            degree++;
+            mainContent.style.opacity = String(degree / 5);
+            if (degree === 5) {
+                clearInterval(interval);
+            }
+        }, 50);
+    }
 }
 
 window.addEventListener("scroll", changeNavbarShadow);
